65895da40003c59c53c1513a0c43443935967ee8
MerriBoachie <merriettaboachie@gmail.com>
2022-08-13 22:30:48 -0400

    Add files via upload

---
+++ Corgi Space Adventure Game/Project Files/Corgi Space Adventure (Project Files)/frmGameOver.vb
@@ -0,0 +1,23 @@
+﻿'Game over Screen Code
+
+Public Class frmGameOver
+
+    'Public variable to get score from game form
+    Public intScore As Integer
+
+    Private Sub btnTitle_Click(sender As Object, e As EventArgs) Handles btnTitle.Click
+
+        'Hide form if player clicks Back To Title
+        Me.Hide()
+
+    End Sub
+
+    Private Sub frmGameOver_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Set intScore value to score value from game form
+        intScore = frmGame.intScoreHold
+
+        'Display this in label
+        lblScoreIs.Text = "Your score is " & intScore & "."
+
+    End Sub
+End Class
